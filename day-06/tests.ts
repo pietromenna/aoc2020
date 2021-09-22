2fcc4d9e12a28333c796dc6221da606a9403bee8
Pietro Menna <pietromenna@gmail.com>
2021-09-21 22:25:58 -0300

    Day 6 - part two

--- day-06/tests.ts
+++ day-06/tests.ts
@@ -3,9 +3,9 @@ import * as fs from "fs";
 
 
 const firstExample :string = "abcx\nabcy\nabcz";
-const secondExample :string = "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb\n";
+const secondExample :string = "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb";
 
-function partOneEachGroup(input :string) :number {
+function partOne(input :string) :number {
     // break into array of strings separated
     const lines = input.split("\n");
     const questions :Set<string>= new Set();
@@ -22,27 +22,58 @@ function partOneEachGroup(input :string) :number {
     return count;
 }
 
-function partOne(input :string) :number {
+function processByGroup(input :string, f: (i: string) => number ) :number {
     let count = 0;
     const groups = input.split("\n\n");
 
     groups.forEach((g) => {
-        count += partOneEachGroup(g);
+        count += f(g);
     });
+
+    return count;
+}
+
+function partTwo(input :string) :number {
+    // break into array of strings separated
+    const lines = input.split("\n");
+    const questions :Map<string, any>= new Map();
+    let count = 0;
+
+    lines.forEach((line) => {
+        for(let i=0; i< line.length; i++){
+            if (questions.has(line[i])) {
+                questions.set(line[i], questions.get(line[i])+1);
+            } else {
+                questions.set(line[i],1);
+            }
+        };
+    });
+
+    questions.forEach((_, v) => {
+        if (questions.get(v) === lines.length) count += 1;
+    })
 
     return count;
 }
 
 describe("Day Six", () => {
     it("Part one, first example", () => {
-        expect(partOne(firstExample)).to.eq(6);
+        expect(processByGroup(firstExample, partOne)).to.eq(6);
     });
 
     it("Part one, second example", () => {
-        expect(partOne(secondExample)).to.eq(11);
+        expect(processByGroup(secondExample, partOne)).to.eq(11);
     });
 
     it("Part One, my input", () => {
-        expect(partOne(fs.readFileSync("./day-06/input.txt").toString('utf-8'))).to.eq(6521);
-    })
+        expect(processByGroup(fs.readFileSync("./day-06/input.txt").toString('utf-8'), partOne)).to.eq(6521);
+    });
+
+    it("Part Two, example",() => {
+        expect(processByGroup(secondExample, partTwo)).to.eq(6);
+    });
+
+    it("Part Two, my input",() => {
+        expect(processByGroup(fs.readFileSync("./day-06/input.txt").toString('utf-8'), partTwo)).to.eq(3305);
+    });
 });
