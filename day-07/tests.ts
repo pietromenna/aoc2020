a97e755027dafe42d27a22a78fa95155fde85287
Pietro Menna <pietromenna@gmail.com>
2021-10-12 00:47:01 -0300

    Day 7 - Part Two

--- day-07/tests.ts
+++ day-07/tests.ts
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import * as fs from "fs";
 
 const sampleInput = "light red bags contain 1 bright white bag, 2 muted yellow bags.\ndark orange bags contain 3 bright white bags, 4 muted yellow bags.\nbright white bags contain 1 shiny gold bag.\nmuted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\nshiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\ndark olive bags contain 3 faded blue bags, 4 dotted black bags.\nvibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\nfaded blue bags contain no other bags.\ndotted black bags contain no other bags.";
+const sampleInput2 = "shiny gold bags contain 2 dark red bags.\ndark red bags contain 2 dark orange bags.\ndark orange bags contain 2 dark yellow bags.\ndark yellow bags contain 2 dark green bags.\ndark green bags contain 2 dark blue bags.\ndark blue bags contain 2 dark violet bags.\ndark violet bags contain no other bags.";
 
 interface Bag {
     name: string,
@@ -55,6 +56,21 @@ function partOne(input: string, bag :string) :number {
     return count;
 }
 
+function containsBags(name :string, bags : Map<string, Bag>) :number {
+    let count = 1;
+    const b = bags.get(name);
+    for (const k in b?.children) {
+        count += b?.children[k]! * containsBags(k,bags);
+    }
+    return count;
+}
+
+function partTwo(input: string, bag :string) :number {
+    const bags = parseBags(input);
+
+    return containsBags(bag, bags) - 1 ;
+}
+
 describe("Day Seven", () => {
     it("Part one, example", () => { 
         expect(partOne(sampleInput,"shiny gold")).to.eq(4);
@@ -62,5 +78,17 @@ describe("Day Seven", () => {
 
     it("Part one, my input", () => { 
         expect(partOne(fs.readFileSync("./day-07/input.txt").toString('utf-8'),"shiny gold")).to.eq(289);
+    });
+
+    it("part two, example 1", () => {
+        expect(partTwo(sampleInput,"shiny gold")).to.eq(32);
+    });
+
+    it("part two, example 2", () => {
+        expect(partTwo(sampleInput2,"shiny gold")).to.eq(126);
+    });
+
+    it("part two, my input", () => {
+        expect(partTwo(fs.readFileSync("./day-07/input.txt").toString('utf-8'),"shiny gold")).to.eq(30055);
     });
 });
